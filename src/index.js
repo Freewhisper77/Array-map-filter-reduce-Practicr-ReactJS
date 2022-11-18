dc21c45cef2a566a5b30ad7b174a1b0138bdac66
Freewhisper77 <emmasddx@foxmail.com>
2022-11-18 14:43:57 +0800

    map, filter, reduce

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 var numbers = [3, 56, 2, 48, 5];
 
 //Map -Create a new array by doing something with each item in an array.
+const newNumbers = numbers.map((x) => x * 2);
 
 //Filter - Create a new array by keeping the items that return true.
+const filteredNumbers = numbers.filter((x) => x >=5 );
 
 //Reduce - Accumulate a value by doing something to each item in an array.
+const sum = numbers.reduce((x,y) => x + y, 0);
 
 //Find - find the first item that matches from an array.
 
