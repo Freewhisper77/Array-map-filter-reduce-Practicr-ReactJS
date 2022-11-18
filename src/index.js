0104ae96f910424097cecf6f608cb5b4f6096a31
Freewhisper77 <emmasddx@foxmail.com>
2022-11-18 15:04:43 +0800

    feat: put [meaning] in emojipedia to a new array

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
-var numbers = [3, 56, 2, 48, 5];
+//Task:get [meaning] in emojipedia then put them to a new array & limit the length of meaning = 100.
 
-//Map -Create a new array by doing something with each item in an array.
-const newNumbers = numbers.map((x) => x * 2);
+import emojipedia from "./emojipedia";
 
-//Filter - Create a new array by keeping the items that return true.
-const filteredNumbers = numbers.filter((x) => x >=5 );
-
-//Reduce - Accumulate a value by doing something to each item in an array.
-const sum = numbers.reduce((x,y) => x + y, 0);
-
-//Find - find the first item that matches from an array.
-// diff in Filter & Find: When Find get the return, it will not go through the remaining array.
-const num = numbers.find((x) => x > 5); //56
-
-//FindIndex - find the index of the first item that matches.
-const i = numbers.findIndex((x) => x > 5); //1
+const newArray = emojipedia.map((item) => item.meaning.substring(0,100));
